92995d163712337816357ba790548f590ea998c2
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-07-19 13:36:57 +0100

    feat(auth): add AuthResponse model and AuthService for authentication logic

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { AuthResponse } from '../models/auth.model';
+
+@Injectable({
+  providedIn: 'root' // Disponible partout dans l'application
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/auth'; // L'URL de ton Spring Boot
+
+  login(credentials: { username: string; password: string }): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login`, credentials).pipe(
+      tap(response => {
+        localStorage.setItem('token', response.access_token);
+        localStorage.setItem('refresh_token', response.refresh_token);
+      })
+    );
+  }
+
+  register(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('access_token');
+  }
+
+  // Se déconnecter
+  logout(): void {
+    localStorage.removeItem('access_token');
+  }
+}
