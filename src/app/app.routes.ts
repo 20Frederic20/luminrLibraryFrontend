bae32dafe5f0e427b9a4f54b1119f77e347ca59b
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-07-19 02:48:44 +0100

    feat(auth): implement login and register components
    Add login and registration functionality including components, templates,
    styles, and unit tests. Update application routing to include the new
    authentication paths.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home'
+import { Login } from './features/auth/login/login'
+import { Register } from './features/auth/register/register'
 
 export const routes: Routes = [
   {
     path: 'home',
     component: Home
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: 'register',
+    component: Register
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
   }
 ];
