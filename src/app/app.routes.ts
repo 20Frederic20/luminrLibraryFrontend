7fbfd91a664bc875e707aaf315c155f994334f20
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-07-20 10:22:48 +0100

    refactor(router): restructure application routes and implement lazy loading
    Reorganize the routing configuration to improve modularity and performance.
    The changes include:
    
    - Implement lazy loading for auth and dashboard components using
      `loadComponent`
    - Group authentication routes under an `auth` prefix
    - Set the root path to redirect to `auth/login`
    - Add a wildcard route to redirect unknown paths to the login page
    - Remove deprecated direct component imports in favor of dynamic imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,37 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home'
-import { Login } from './features/auth/login/login'
-import { Register } from './features/auth/register/register'
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    component: Home
-  },
-  {
-    path: 'login',
-    component: Login
-  },
-  {
-    path: 'register',
-    component: Register
-  },
+  // 1. Si l'utilisateur ne tape rien, on le redirige directement vers le login
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: 'auth/login',
     pathMatch: 'full'
+  },
+
+  // 2. Groupe "auth" : Toutes les routes d'authentification sont regroupées ici
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login').then(m => m.Login)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register').then(m => m.Register)
+      }
+    ]
+  },
+
+  // 3. Espace connecté : Le tableau de bord principal
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard//dashboard').then(m => m.Dashboard)
+  },
+
+  // 4. Page inconnue (Erreur 404) : Redirection de sécurité vers le login
+  {
+    path: '**',
+    redirectTo: 'auth/login'
   }
 ];
